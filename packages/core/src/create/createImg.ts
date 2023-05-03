f9597c0fad9d8c6e841932e7c6753c304ce80242
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-03 17:42:17 +0800

    feat: the function to create IMG

--- packages/core/src/create/createImg.ts
+++ packages/core/src/create/createImg.ts
@@ -1,14 +1,22 @@
-export const createImg = (parentNode: FrameNode) => {
-  const res = figma.createRectangle();
-  const { width, height } = parentNode;
-  res.x = 0;
-  res.y = 0;
-  res.resize(width, height);
-  res.fills = [
-    {
-      type: "SOLID",
-      color: { r: 0, g: 0, b: 1 },
-    },
-  ];
-  return res;
+export const createImg = async (data: Uint8Array, parentNode: FrameNode) => {
+  try {
+    const img = figma.createImage(data);
+
+    const res = figma.createRectangle();
+    const { width, height } = parentNode;
+    res.x = 0;
+    res.y = 0;
+    res.resize(width, height);
+    res.fills = [
+      {
+        type: "IMAGE",
+        imageHash: img.hash,
+        scaleMode: "FILL",
+      },
+    ];
+    return res;
+  } catch (error) {
+    console.error(error);
+    return;
+  }
 };
