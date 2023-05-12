1df2cf58eb914b34934cea8f8cdfdaebb862655a
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-12 09:47:02 +0800

    feat: update the submit for prompt

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,20 +1,39 @@
 import { useState } from "react";
+import axios from "axios";
+
+import { actions, INode } from "figma-vite-common/constants";
+import helpers from "./utils";
+
 import "./App.scss";
+const url = "http://127.0.0.1:3000/api";
+
 function App() {
-  //   const url = "";
   const [prompt, setPrompt] = useState<string>("");
 
-  const inputPrompt = (text: string) => {
+  const handlePrompt = (text: string) => {
     setPrompt(text);
   };
 
-  //   const submitPrompt = () => {
-  //     console.log("submit");
-  //   };
+  const fetchData = async () => {
+    try {
+      const res = await axios.post(url, { prompt });
+      return res.data;
+    } catch (error) {
+      throw new Error(error as string);
+    }
+  };
 
-  const create = () => {
+  const submitPrompt = async () => {
+    console.log("submit");
+    const res = await helpers.retryFn<INode[]>(fetchData, 3, 1500);
+    console.log(res);
+
+    create(res);
+  };
+
+  const create = (data: INode[]) => {
     window.parent.postMessage(
-      { pluginMessage: { type: "create-rectangles", prompt } },
+      { pluginMessage: { type: actions.CREATE_PAGES, data } },
       "*"
     );
   };
@@ -27,9 +46,9 @@ function App() {
     <section className="main-page">
       <h2>Figma 网页生成</h2>
       <h3>Prompt: </h3>
-      <textarea onChange={(e) => inputPrompt(e.target.value)} />
+      <textarea onChange={(e) => handlePrompt(e.target.value)} value={prompt} />
       <div className="button-group">
-        <button id="create" onClick={create}>
+        <button id="create" onClick={submitPrompt}>
           Create
         </button>
         <button id="cancel" onClick={cancel}>
